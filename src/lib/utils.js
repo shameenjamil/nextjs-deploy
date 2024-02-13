8065ff186771356799674e4150c0f3dc7f8849f8
shameen <shameen.jamil@prixite.com>
2024-02-13 18:42:08 +0500

    deploy v5

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,17 +1,17 @@
-import mongoose from "mongoose"
+// import mongoose from "mongoose"
 
-const connection = {};
+// const connection = {};
 
-export const connectToDb = async () => {
-  try {
-    if(connection.isConnected) {
-      console.log("Using existing connection");
-      return;
-    }
-    const db = await mongoose.connect(process.env.MONGO);
-    connection.isConnected = db.connections[0].readyState;
-  } catch (error) {
-    console.log(error);
-    throw new Error(error);
-  }
-};
+// export const connectToDb = async () => {
+//   try {
+//     if(connection.isConnected) {
+//       console.log("Using existing connection");
+//       return;
+//     }
+//     const db = await mongoose.connect(process.env.MONGO);
+//     connection.isConnected = db.connections[0].readyState;
+//   } catch (error) {
+//     console.log(error);
+//     throw new Error(error);
+//   }
+// };
