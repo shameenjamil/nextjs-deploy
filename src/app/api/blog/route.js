6fd6a4f66b70f27422c66adc8960878db4029d8b
shameen <shameen.jamil@prixite.com>
2024-02-13 18:24:44 +0500

    deploy v3

--- src/app/api/blog/route.js
+++ src/app/api/blog/route.js
@@ -1,15 +1,15 @@
-import { Post } from "@/lib/models";
-import { connectToDb } from "@/lib/utils";
-import { NextResponse } from "next/server";
+// import { Post } from "@/lib/models";
+// import { connectToDb } from "@/lib/utils";
+// import { NextResponse } from "next/server";
 
-export const GET = async () => {
-  try {
-    connectToDb();
+// export const GET = async () => {
+//   try {
+//     connectToDb();
 
-    const posts = await Post.find();
-    return NextResponse.json(posts);
-  } catch (err) {
-    console.log(err);
-    throw new Error("Failed to fetch posts!");
-  }
-};
+//     const posts = await Post.find();
+//     return NextResponse.json(posts);
+//   } catch (err) {
+//     console.log(err);
+//     throw new Error("Failed to fetch posts!");
+//   }
+// };
